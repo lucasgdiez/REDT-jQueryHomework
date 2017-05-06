567ebdfbd5266986e154369542243e5d9b9357d8
Lucas <lucasgdiez@gmail.com>
2017-05-06 06:28:03 +0200

    updated markup/correct way

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
-(function(){
-    //Write here your js alchemy ;)
-})();
+$("#button1").on("click", function(e){
+    var TC = $("#button1").val();
+    e.preventDefault();
+    generateTable(TC);
+});
+
+function generateTable(count) {
+	var auxHTML = "";
+	var tWrap = $("#tabla tWrap");
+	for (var i = 0; i < count; i++) {
+		var auxHTML = "<tr><td>" + i + "</td><td> elemento" + i "</td> </tr>";
+		tWrap.append(auxHTML);
+	}
+}
